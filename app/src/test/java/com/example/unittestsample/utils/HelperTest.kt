1a8c2d3373e3076b07cb07ff9be281ce630fd4ae
Vishal Gupta <vishal.gupta@kellton.com>
2024-01-11 21:33:01 +0530

    Multiple test cases

--- app/src/test/java/com/example/unittestsample/utils/HelperTest.kt
+++ app/src/test/java/com/example/unittestsample/utils/HelperTest.kt
@@ -1,16 +1,37 @@
 package com.example.unittestsample.utils
 
+import org.junit.After
 import org.junit.Assert.*
+import org.junit.Before
 
 import org.junit.Test
 
 class HelperTest {
 
+    lateinit var helper: Helper
+
+    @Before
+    fun setup(){
+        helper = Helper()
+    }
+
+    @After
+    fun testFinish(){
+        println("test case finished")
+    }
+
     @Test
     fun checkPalindrome() {
 
-        val helper = Helper()
+
         val result = helper.checkPalindrome("abba")
         assertEquals( true, result)
+    }
+
+    @Test
+    fun checkPalindrome_input_levels_expected_false() {
+
+        val result = helper.checkPalindrome("levels")
+        assertEquals( false, result)
     }
 }
