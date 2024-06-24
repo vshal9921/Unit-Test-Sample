0670e0b3b6b1eb68c2cbbc2211f5bf1dda5ee22c
Vishal Gupta <vshal9921@gmail.com>
2024-06-24 16:27:25 +0530

    Espresso - check intent

--- app/src/androidTest/java/com/example/unittestsample/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/unittestsample/MainActivityTest.kt
@@ -1,11 +1,16 @@
 package com.example.unittestsample
 
+import android.content.Intent
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.activityScenarioRule
+import org.hamcrest.core.AllOf.allOf
 import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
@@ -23,5 +28,14 @@ class MainActivityTest{
         onView(withId(R.id.nextBtn)).perform(click())
         onView(withId(R.id.nextBtn)).perform(click())
         onView(withId(R.id.quoteText)).check(matches(withText("Moon Walk")))
+    }
+
+    @Test
+    fun testShareButton_expected_intentChooser(){
+        Intents.init()
+        val expected = allOf(hasAction(Intent.ACTION_SEND))
+        onView(withId(R.id.floatingActionButton)).perform(click())
+        intended(expected)
+        Intents.release()
     }
 }
